438cff32590bece24cd745b21cb9e70aef89801a
wangshuo <you@example.com>
2018-12-06 19:18:53 +0800

    新成员修改了test2

--- src/test2.java
+++ src/test2.java
@@ -2,4 +2,8 @@
  * Created by 123123 on 2018/12/5.
  */
 public class test2 {
+    public static void main(String[] args) {
+        System.out.println("新成员1的test1.main");
+        System.out.println("222222222");
+    }
 }
